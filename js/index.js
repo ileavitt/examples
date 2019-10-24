bb8d2975c07a421f6ac73528b5a4429132808a78
ileavitt <ianjleavitt@gmail.com>
2019-10-24 16:17:22 -0700

    don't need this file

--- js/index.js
+++
@@ -1,6 +0,0 @@
-// $(document).ready(() => {
-//     $(document.body).on('click', '.card[data-click=true]', (e) => {
-//         const href = $(e.currentTarget).data('href');
-//         window.location = href;
-//     });
-// });
