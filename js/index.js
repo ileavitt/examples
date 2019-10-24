acefe0f5ddb96c5a985cb5e2393e9db522669676
ileavitt <ianjleavitt@gmail.com>
2019-10-24 16:03:50 -0700

    initial commit

---
+++ js/index.js
@@ -0,0 +1,6 @@
+// $(document).ready(() => {
+//     $(document.body).on('click', '.card[data-click=true]', (e) => {
+//         const href = $(e.currentTarget).data('href');
+//         window.location = href;
+//     });
+// });
